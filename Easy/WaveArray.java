d672326b9c3abcec966ce8d0ba9681bf999986e3
PriyadarshanCoder <tuplondhepriyadarshan@gmail.com>
2025-07-31 12:06:31 +0530

    Update WaveArray.java

--- Easy/WaveArray.java
+++ Easy/WaveArray.java
@@ -1,1 +1,13 @@
-1	Basic	https://practice.geeksforgeeks.org/problems/wave-array-1587115621/1/?track=amazon-arrays&batchId=192	Array (leetcode link)
+class Solution {
+    public void sortInWave(int arr[]) {
+        // code here
+         int n = arr.length;
+        
+        for (int i = 0; i < n - 1; i += 2) {
+            // Swap adjacent elements
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+        }
+    }
+}
