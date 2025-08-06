ad44c9da4053b5dd4fb210dabdf4ee2d3c5e7c4b
Priyadarshn Tuplondhaay <tuplondhepriyadarshan@gmail.com>
2025-08-06 11:57:12 +0530

    Create PermuteUnique2.java

---
+++ Medium/PermuteUnique2.java
@@ -0,0 +1,28 @@
+public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);  // sort to handle duplicates
+        boolean[] used = new boolean[nums.length];
+        backtrack(nums, used, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> temp, List<List<Integer>> res) {
+        if (temp.size() == nums.length) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            // skip used numbers
+            if (used[i]) continue;
+
+            // skip duplicates: if current num == previous and previous not used, skip
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+            backtrack(nums, used, temp, res);
+            temp.remove(temp.size() - 1);
+            used[i] = false;
+        }
+    }
