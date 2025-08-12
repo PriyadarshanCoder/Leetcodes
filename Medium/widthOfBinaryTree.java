c17f77a476a1085bedb15f56ece7b52051027e29
Priyadarshn Tuplondhaay <tuplondhepriyadarshan@gmail.com>
2025-08-12 10:14:41 +0530

    Create widthOfBinaryTree.java

---
+++ Medium/widthOfBinaryTree.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        int maxWidth = 0;
+        Deque<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0L)); // use long to avoid overflow
+
+        while (!queue.isEmpty()) {
+            int currentLevelSize = queue.size();
+            long startIndex = queue.peekFirst().index; // first element
+            long endIndex   = queue.peekLast().index;  // last element
+
+            maxWidth = Math.max(maxWidth, (int)(endIndex - startIndex + 1));
+
+            for (int i = 0; i < currentLevelSize; i++) {
+                Pair current = queue.pollFirst();
+                long idx = current.index;
+
+                if (current.node.left != null)
+                    queue.offerLast(new Pair(current.node.left, idx * 2));
+                if (current.node.right != null)
+                    queue.offerLast(new Pair(current.node.right, idx * 2 + 1));
+            }
+        }
+
+        return maxWidth;
+    }
+
+    static class Pair {
+        TreeNode node;
+        long index;
+        Pair(TreeNode n, long i) {
+            node = n;
+            index = i;
+        }
+    }
+}
+
+// TreeNode definition
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
